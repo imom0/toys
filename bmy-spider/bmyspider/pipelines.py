18527ce34edbd733852b17ed9416073dcf8c2c88
imom0 <mobeiheart@gmail.com>
2013-12-19 20:26:37 +0800

    Add duplicate pipeline

--- bmy-spider/bmyspider/pipelines.py
+++ bmy-spider/bmyspider/pipelines.py
@@ -2,7 +2,18 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exceptions import DropItem
+
 
 class BmySpiderPipeline(object):
+
+    def __init__(self):
+        self.seen = set()
+
     def process_item(self, item, spider):
-        return item
+        ident = (item['author'], item['date'])
+        if ident in self.seen:
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.seen.add(ident)
+            return item
--- bmy-spider/bmyspider/settings.py
+++ bmy-spider/bmyspider/settings.py
@@ -13,3 +13,7 @@ NEWSPIDER_MODULE = 'bmyspider.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'bmyspider (+http://www.yourdomain.com)'
+
+ITEM_PIPELINES = {
+    'bmyspider.pipeline.BmySpiderPipeline': 100,
+}
