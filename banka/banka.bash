fe8061caacb0ab99f7d7d6670901b506488aae74
imom0 <mobeiheart@gmail.com>
2014-01-11 22:23:01 +0800

    Use function

--- banka/banka.bash
+++ banka/banka.bash
@@ -4,25 +4,32 @@ SOUNDS_DIR="/System/Library/Sounds/"
 WORK_INTERVAL=25
 REST_INTERVAL=5
 
+function alert {
+	if [ "$2" != "silent" ]; then
+		say $1 &
+	fi
+	echo $1
+}
+
 # work
-say "start work"
+alert "start work"
 sleep 60
 for i in `seq $(($WORK_INTERVAL-1)) 1`
 do
 	if [ $(($i % 5)) -eq "0" ]; then
-		say "$i minutes left" &
+		alert "$i minutes left"
 	else
 		say -v Whisper tick tick &
+		alert "$i minutes left" silent
 	fi
-	echo "$i minutes left"
 	sleep 60
 done
 afplay "$SOUNDS_DIR"Ping.aiff
-sleep 3
-say "work finished"
-sleep 2
+sleep 1
+alert "work finished"
+sleep 1
 
 # rest
-say "start rest"
+alert "start rest"
 sleep $(($REST_INTERVAL*60))
-say "rest finished"
+alert "rest finished"
