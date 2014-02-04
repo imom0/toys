0bb54b444f21b8af708ca2e1b0bf7317bd8e2e17
imom0 <mobeiheart@gmail.com>
2014-02-05 04:07:29 +0800

    Add douban-clojure-client

---
+++ douban-clojure-client/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to douban-clojure-client
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ douban-clojure-client/test/douban_clojure_client/core_test.clj
@@ -0,0 +1,7 @@
+(ns douban-clojure-client.core-test
+  (:require [clojure.test :refer :all]
+            [douban-clojure-client.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
